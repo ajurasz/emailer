7ebc98da4dcc2d8b686fb0c12ad2ec7271048912
ajurasz <arkadiusz.jurasz@gmail.com>
2018-08-05 19:32:59 +0200

    add logging to resolve some heroku issue

--- src/index.js
+++ src/index.js
@@ -42,6 +42,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
+    console.log('catch all route');
+    console.log(
+      `looking for ${req.protocol}://${req.get('host')}${req.originalUrl}`
+    );
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
