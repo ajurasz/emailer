a2f2c2e519d1e34a3a10da1adccc0bbacee38cac
ajurasz <arkadiusz.jurasz@gmail.com>
2018-08-05 18:58:12 +0200

    server and client adjustments for production build on heroku

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import passport from 'passport';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+import path from 'path';
 
 import './services/passport';
 import checkAuth from './helpers/checkAuth';
@@ -24,18 +25,26 @@ mongoose
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [process.env.COOKIE_KEY]
   })
 );
-app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/auth', auth);
 app.use('/api/user', checkAuth, user);
 app.use('/api/wallet', checkAuth, wallet);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
