210544bf484a25c9899dada5d16793e8a59abbfa
ajurasz <arkadiusz.jurasz@gmail.com>
2018-08-05 19:19:25 +0200

    inform strategy to trust heroku proxy

--- src/services/passport.js
+++ src/services/passport.js
@@ -18,7 +18,8 @@ passport.use(
     {
       clientID: process.env.AUTH_GOOGLE_CLIENT_ID,
       clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (token, refreshToken, profile, done) => {
       const id = profile.id;
