d6217d1b1b93cfac5027d16e10a23810052a60e0
ajurasz <arkadiusz.jurasz@gmail.com>
2018-08-17 11:48:01 +0200

    transform received events

--- server/routes/feedback.js
+++ server/routes/feedback.js
@@ -3,8 +3,6 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/:surveyId/:choice', (req, res) => {
-  const { surveyId, choice } = req.params;
-  console.log(surveyId, choice);
   res.redirect(`${process.env.DOMAIN}/thanks`);
 });
 
--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -1,9 +1,34 @@
 import express from 'express';
+import { URL } from 'url';
+import UrlPattern from 'url-pattern';
 
 const router = express.Router();
+const pattern = new UrlPattern('/feedback/:surveyId/:choice');
 
 router.post('/', (req, res) => {
+  const events = (req.body || [])
+    .filter(onlyClicks)
+    .map(extract)
+    .filter(removeEmpty);
+
+  console.log(events);
   res.status(200).send();
 });
+
+const onlyClicks = ({ event }) => event === 'click';
+
+const extract = ({ email, url }) => {
+  console.log(url);
+  const match = pattern.match(new URL(url).pathname);
+  console.log(match);
+  if (match) {
+    return {
+      email,
+      ...match
+    };
+  }
+};
+
+const removeEmpty = event => event;
 
 export default router;
